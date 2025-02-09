8b212f0bd1cca5b8952c76d6bc5d5d1e04b8d599
Aleksandr802 <savs802@gmail.com>
2025-02-09 13:53:37 -0800

    Initial Commit

---
+++ TempS3Data/TempS3DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempS3DataApp.swift
+//  TempS3Data
+//
+//  Created by Oleksandr Seminov on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TempS3DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TempS3DataTests/TempS3DataTests.swift
@@ -0,0 +1,17 @@
+//
+//  TempS3DataTests.swift
+//  TempS3DataTests
+//
+//  Created by Oleksandr Seminov on 2/9/25.
+//
+
+import Testing
+@testable import TempS3Data
+
+struct TempS3DataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
